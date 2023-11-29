c0c0ab975070d0f3b6dcbf47b5f1efcbc4620ea3
Caio Pegorini <131922476+CaioPegorini@users.noreply.github.com>
2023-11-28 23:31:47 -0300

    Add files via upload

--- Pet_Rescue/pet_recue_db.sql
+++ Pet_Rescue/pet_recue_db.sql
@@ -1,5 +1,6 @@
 create database if not exists pet_rescue_db;
 use pet_rescue_db;
+
 create table if not exists users (
 	id tinyint unsigned primary key auto_increment,
     firstname varchar(45) not null,
@@ -10,4 +11,36 @@ create table if not exists users (
     password char(30) not null
 );
 
-select * from users
+create table if not exists OwnerlessAnimal (
+	id tinyint unsigned primary key auto_increment,
+    specie varchar(30) not null,
+    age tinyint not null,
+    description text not null,
+    additionalinfo text not null
+);
+
+create table if not exists LostPet (
+	id tinyint unsigned primary key auto_increment,
+    pet_owner varchar(110) not null,
+    petname varchar(110) not null,
+    specie varchar(30) not null,
+    age tinyint not null,
+    description text not null,
+    additionalinfo text not null,
+    constraint fk_pet_owner foreign key (pet_owner) references users(id)
+);
+
+create table if not exists institution (
+	id tinyint unsigned primary key auto_increment,
+    institutename varchar(110) not null,
+    institutetype varchar(110) not null,
+    responsible varchar(110) not null,
+    contact varchar(13) not null,
+    constraint fk_responsible foreign key (responsible) references users(id),
+    constraint fk_contact foreign key (contact) references users(number)
+);
+
+select * from users;
+select * from OwnerlessAnimal;
+select * from LostPet;
+select * from institution
